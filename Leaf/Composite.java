5583132df3543c735fffceefb1a5b0caf384f1cc
HongKai Wang <120489830+7JU7Y7@users.noreply.github.com>
2023-07-10 21:03:33 +0800

    Revert "Update Composite.java"
    This reverts commit 691e6c320949ef3bc313df140d9d5eadd89860c8.

--- Leaf/Composite.java
+++ Leaf/Composite.java
@@ -16,7 +16,6 @@ public class Composite implements Component{
     @Override
     public int count() {
         int sum = 0;
-        int a = 0;
         for(Component counter : counterList){
             sum += counter.count();
         }
