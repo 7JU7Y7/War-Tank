4b153cff58bf70982fdf3476fd28d4f4eaecee5a
HongKai Wang <120489830+7JU7Y7@users.noreply.github.com>
2023-07-10 17:31:16 +0800

    Add files via upload

---
+++ Leaf/Composite.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Composite implements Component{
+    private ArrayList<Component> counterList = new ArrayList<>();
+
+    public void add(Component counter){
+        counterList.add(counter);
+    }
+    public void remove(Component counter){
+        counterList.remove(counter);
+    }
+    public ArrayList<Component> getChild(){
+        return counterList;
+    }
+
+    @Override
+    public int count() {
+        int sum = 0;
+        for(Component counter : counterList){
+            sum += counter.count();
+        }
+        return sum;
+    }
+}
